739c5bb5401cb3464638fc97cc6a172f715834f5
Billiesnyc <billiesnyc@gmail.com>
2018-10-22 16:30:45 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+class API {
+    static init () {
+      this.baseUrl = 'http://localhost:3000/'
+      this.usersUrl = `${this.baseUrl}/users`
+      this.patternsUrl = `${this.baseUrl}/patterns`
+    }
+  
+    static getUsers() {
+      return fetch(this.usersUrl)
+        .then(resp => resp.json())
+    }
+  
+    static getUser (id) {
+      return fetch(`${this.usersUrl}/${id}`)
+        .then(resp => resp.json())
+    }
+
+    
+  
+    // static createQuote (quote, author, likes) {
+    //   // 'POST'
+    //   return fetch(this.baseUrl, {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //       'quote': quote,
+    //       'author': author,
+    //       'likes': likes
+    //     })
+    //   }).then(resp => resp.json())
+    //   .then(renderQuote)
+    // }
+  
+    // static deleteQuote (id) {
+    //   return fetch(`${this.baseUrl}/${id}`, {
+    //     method: 'DELETE'
+    //   })
+    // }
+  
+    // static increaseLikes (id, increasedLike) {
+    //   return fetch(`${this.baseUrl}/${id}`, {
+    //     method: 'PATCH',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ 'likes': increasedLike })
+    //   }).then(resp => resp.json())
+    // }
+  
+    // static editQuote (quote) {
+    //   return fetch(`${this.baseUrl}/${quote.id}`, {
+    //     method: 'PATCH',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(quote)
+    //   }).then(resp => resp.json())
+    // }
+  }
+  
+  API.init()
