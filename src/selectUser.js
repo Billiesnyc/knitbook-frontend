d4622bb9e7d698d9757e97fcdfea33cb8b04c7a9
Billiesnyc <billiesnyc@gmail.com>
2018-10-24 13:13:22 +0100

    filter button added

--- src/selectUser.js
+++ src/selectUser.js
@@ -1,4 +1,7 @@
 const selectTagUsers = document.querySelector('#userSelect')
+const selectDifficulty = document.querySelector('#difficultySelect')
+const selectSize = document.querySelector('#sizeSelect')
+
 // const allUsers = []
 
 
@@ -8,6 +11,28 @@ const selectTagUsers = document.querySelector('#userSelect')
 //     })
 // })
 
+function unique (array) {
+  return array.filter(function(a){
+    return !this[a] ? this[a] = true : false;
+  }, {});
+}
+
+function difficultyUnique (patterns){
+    let difficultyArray = [];
+    patterns.forEach (pattern => {
+        difficultyArray.push(pattern.difficulty)
+    })
+   return unique(difficultyArray);
+}
+
+function sizeUnique (patterns){
+    let sizeArray = [];
+    patterns.forEach (pattern => {
+        sizeArray.push(pattern.size)
+    })
+   return unique(sizeArray);
+}
+
 function createUserOptions(users, select){
     users.forEach(userOpt => {
     const options = document.createElement('option')
@@ -16,4 +41,24 @@ function createUserOptions(users, select){
     })
 }
 
+function createDifficultyOptions(patterns, select){
+    const patternDifficulty = difficultyUnique(patterns)
+    patternDifficulty.forEach(patternOpt => {
+    const options = document.createElement('option')
+    options.innerHTML = `<option value="${patternOpt}">${patternOpt}</option>`
+    select.appendChild(options)
+    })
+}
+
+function createSizeOptions(patterns, select){
+    const patternSize = sizeUnique(patterns)
+    patternSize.forEach(patternOpt => {
+    const options = document.createElement('option')
+    options.innerHTML = `<option value="${patternOpt.id}">${patternOpt}</option>`
+    select.appendChild(options)
+    })
+}
+
 API.getUsers().then(users =>  createUserOptions(users, selectTagUsers))
+API.getPatterns().then(patterns =>  createDifficultyOptions(patterns, selectDifficulty))
+API.getPatterns().then(patterns =>  createSizeOptions(patterns, selectSize))
