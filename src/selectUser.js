1f06f7e52f6aa073c901513de22499d3883d484b
github username <github email address>
2018-10-24 10:47:20 +0100

    select user different file missing css

---
+++ src/selectUser.js
@@ -0,0 +1,19 @@
+const selectTagUsers = document.querySelector('#userSelect')
+// const allUsers = []
+
+
+// API.getUsers().then(users => {
+//     users.forEach(user => {
+//         allUsers.push(user)
+//     })
+// })
+
+function createUserOptions(users, select){
+    users.forEach(userOpt => {
+    const options = document.createElement('option')
+    options.innerHTML = `<option value="${userOpt.id}">${userOpt.name}!</option>`
+    select.appendChild(options)
+    })
+}
+
+API.getUsers().then(users =>  createUserOptions(users, selectTagUsers))
