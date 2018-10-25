2eb20fb40dd929e5d739f4ac78d3b1258e0ee2d4
github username <github email address>
2018-10-25 13:38:07 +0100

    add to favorites button

--- src/selectUser.js
+++ src/selectUser.js
@@ -3,15 +3,23 @@ const selectTagUsers = document.querySelector('#userSelect')
 const selectDifficulty = document.querySelector('#difficultySelect')
 const selectSize = document.querySelector('#sizeSelect')
 
-const allUsers = []
+// const UserFavoritesButt = document.querySelector('.all-favorites-popup-button')
+// const addToFavoritesButt = document.querySelector('.add_favorite_button')
 
 
+// function addPatternToFavorite(){
+//     addToFavoritesButt.addEventListener('click', event => {
+//         
+        
+//     })
+// }
 
-API.getUsers().then(users => {
-    users.forEach(user => {
-        allUsers.push(user)
-    })
-})
+// for later when we will need to identify the current user
+// function targetCurrentUser(button, userId){
+//     button.addEventListener('click', event => {
+//         userId = allUsers.filter(user => user.id === selectTagUsers.options.selectedIndex)
+//     })
+// }
 
 function unique (array) {
   return array.filter(function(a){
